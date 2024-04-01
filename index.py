269f9a0ead2ddd1e51b11712a61f77b4ab759630
Guilherme Miranda <125209869+Guicod3@users.noreply.github.com>
2024-04-01 20:45:53 -0300

    Integração de API / Formatação Saída

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 #ENTRADA DE DADOS
 from datetime import datetime #calendário e horas atuais
-import requests from requests
+import requests
 import json
 data = datetime.now()
 dataFormatada = data.strftime('%d/%m/%Y %H:%M')
@@ -13,24 +13,37 @@ horaAtual = datetime.now().hour #Hora atual Int
 saudacaoDoDia = ''
 
 #APICOTACAO
-chaveCotacao = https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL
-responseCot = get(chaveCotacao)
-
-
-
-#COMPILAÇãO E LOGICA
-if horaAtual >= 4 and horaAtual < 12:
-    saudacaoDoDia = 'Bom dia!'
-elif horaAtual >= 12 and horaAtual < 18:
-    saudacaoDoDia = 'Boa tarde!'
+url = 'https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL'
+responseCot = requests.get(url)
+if responseCot.status_code == 200:
+    cotacaoJson = responseCot.json()
+    valorDolar = float(cotacaoJson["USDBRL"]['bid'])
+    valorEuro = float(cotacaoJson["EURBRL"]['bid'])
+    valorBitcoin = float(cotacaoJson["BTCBRL"]['bid'])
 else:
-    saudacaoDoDia = 'Boa noite!'
+    print('[ERRO] Falha na Comunicação API(COTAÇÃO)')
+
+
+#VerificarBOMDIABOATARDEOUBOANOITE
+if horaAtual >= 4 and horaAtual < 12:
+    saudacaoDoDia = 'Bom dia'
+elif horaAtual >= 12 and horaAtual < 18:
+    saudacaoDoDia = 'Boa tarde'
+else:
+    saudacaoDoDia = 'Boa noite'
 
 
 #SAIDA
-print('ATUALIZAÇÕES DO DIA')
-print(f'\n{saudacaoDoDia}, usuário.')
-print(f'\n{diaSemana}, {dataFormatada}')
+print('-------------------------------------------------------------------')
+print('|                       ATUALIZAÇÕES DO DIA                       |')
+print(f'|                 {diaSemana}, {dataFormatada}                 |')
+print('-------------------------------------------------------------------')
+print(f'\n                        {saudacaoDoDia}, usuário.                 ')
+print('\n-------------------------------------------------------------------')
+print('\n                      COTAÇÂO DAS MOEDAS HOJE                      ')
+print(f'        Dólar: R${valorDolar:.2f} | Euro: R${valorEuro:.2f} | Bitcoin: R${valorBitcoin:.2f}')
+print('\n-------------------------------------------------------------------')
+
 
 
 
