1bd337c0028b01932e3d79fc3e79b0812f3f7b7f
Guilherme Miranda <guilmiranda50@gmail.com>
2024-04-01 13:34:25 -0300

    Dia da semana, horário e dia de calendário criado

--- index.py
+++ index.py
@@ -1,4 +1,12 @@
-from datetime import datetime
+from datetime import datetime #calendário e horas atuais
 data = datetime.now()
 dataFormatada = data.strftime('%d/%m/%Y %H:%M')
-print(dataFormatada)
+
+diaSemanaValorBase = datetime.weekday(data)#Mostrar dia da semana
+diaSemanaTextoBase = ('Segunda-Feira', 'Terça-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sábado', 'Domingo')
+diaSemana = diaSemanaTextoBase[diaSemanaValorBase]
+
+print('ATUALIZAÇÕES DO DIA')
+print(f'\n{diaSemana}, {dataFormatada}')
+
+
